1868f48b0ac70976f994afc7b2c41012e72e260e
suxiuwei0809 <suxiuwei0809@outlook.com>
2021-07-04 19:42:48 +0800

    提交代码

---
+++ gulimall-common/src/main/java/com/atguigu/common/constant/AuthServerConstant.java
@@ -0,0 +1,12 @@
+package com.atguigu.common.constant;
+
+/**
+ * <p>Title: AuthServerConstant</p>
+ * Description：
+ *
+ */
+public class AuthServerConstant {
+	public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
+	public static final String LOGIN_USER = "loginUser";
+	public static final String NOT_LOGIN = "请先进行登录";
+}
